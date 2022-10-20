9f726ee198d564a4bf810d88eac0ce854299ae1d
Akhil-Ratn-Singh <59931274+Akhil-Ratn-Singh@users.noreply.github.com>
2022-10-20 12:19:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
+
+
+
+const App = () => {
+  return (
+    <div className="App">App</div>
+  )
+}
+
+
+export default hot(module)(App);
+
